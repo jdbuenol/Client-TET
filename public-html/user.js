aca96b120989fc3e56365d2b1ed786c957b9eb87
jdbuenol <jdbuenol@eafit.edu.co>
2023-04-24 11:28:35 -0400

    Initial push

---
+++ public-html/user.js
@@ -0,0 +1,59 @@
+function signup() {
+    username = $("#signup-username").val()
+    password = $("#signup-password").val()
+    fetch(domain + "/User/new", {
+        method: "POST",
+        mode: "cors",
+        redirect: "follow",
+        body: JSON.stringify({"username": username, "password": password}),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then((res) => {
+        res.json().then((data) => {
+            console.log(data);
+            $("#signup-text").text(JSON.stringify(data))
+        });
+    });
+}
+
+function listUsers() {
+    username = $("#user-username").val()
+    password = $("#user-password").val()
+    fetch(domain + "/User/list?username=" + username + "&password=" + password, {
+        method: "GET",
+        mode: "cors",
+        redirect: "follow"
+    }).then((res) => {
+        res.json().then((data) => {
+            console.log(data);
+            $("#user-text").text(JSON.stringify(data))
+        });
+    })
+}
+
+function deleteUser(){
+    username = $("#user-username").val()
+    password = $("#user-password").val()
+    fetch(domain + "/User/delete", {
+        method: "POST",
+        mode: "cors",
+        redirect: "follow",
+        body: JSON.stringify({"username": username, "password": password}),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then((res) => {
+        res.json().then((data) => {
+            console.log(data);
+            $("#user-text").text(JSON.stringify(data))
+        });
+    });
+}
+
+function usermethods() {
+    choice = $("#user-route").val()
+    if(choice == "list") listUsers();
+    else if(choice == "delete") deleteUser();
+    else $("#user-text").text("Please choose one method")
+}
